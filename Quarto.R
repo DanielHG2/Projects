448432352ae3a76d4ce97032e61ba4c9e6ebbeda
DanielHG2 <alfonsi.1920888@studenti.uniroma1.it>
2024-11-18 23:05:03 +0100

    Add files via upload

---
+++ Quarto.R
@@ -0,0 +1,249 @@
+setwd("C:\\Users\\Daniel192\\Desktop\\TESINA SERIE STORICHE\\ELABORAZIONI\\Quarto-CAViars")
+library(readxl)
+library(openxlsx)
+library(readr)
+library(ggplot2)
+library(PerformanceAnalytics)
+library(expectreg)
+library(rugarch)
+library(quantmod)
+library(GAS)
+ret_crypto_index_comm <- na.omit(read_excel("ret_crypto_index_comm.xlsx"))
+ret_crypto_index_comm$Date <- as.Date(ret_crypto_index_comm$Date)
+calcolo_VAR <- read_excel("calcoloVAR_definitivo.xlsx")
+calcolo_VAR$Date <- as.Date(calcolo_VAR$Date)
+
+Bitcoin <- na.omit(ret_crypto_index_comm$`Bitcoin (BTC)`)
+Bitcoin <- c()
+Bitcoin.ret <- xts(ret_crypto_index_comm[, 2], order.by = ret_crypto_index_comm$Date)
+Bitcoin.ret <- xts(ret_crypto_index_comm[, 2], order.by = ret_crypto_index_comm$Date)
+#GARCH (1,1)
+#NORMALE
+Modello_GARCH11=ugarchspec(mean.model = list(armaOrder = c(0,0)), variance.model = list(model="sGARCH"), distribution.model = 'norm')
+BITCOIN_G11=ugarchfit(data = Bitcoin.ret, spec = Modello_GARCH11)
+FIT_BITCOINm = fitted(BITCOIN_G11)
+FIT_BITCOINs = sigma(BITCOIN_G11)
+####parametro mu
+mu <- min(FIT_BITCOINm)
+
+################################################################
+##############################################################
+############################################################
+
+
+
+####### CON I NUOVI MODELLI: IGm, ASm, SAVm, GJG e GJGm.
+CAViaR.fun = function(beta, ret, tau, model, G) {
+  
+  N = length(ret)
+  VaR = matrix(NA, N, 1)
+  N1 = min(300, N/3)
+  VaR[1] = quantile(ret[1:N1], probs = tau)
+  
+  if (model == "AD") {
+    for(t in 2:N) {
+      VaR[t] = VaR[t-1] + beta * ((1 + exp(G * (ret[t-1] - VaR[t-1])))^(-1) - tau)
+    }
+  } else if (model == "SAV") {
+    for(t in 2:N) {
+      VaR[t] = beta[1] + beta[2] * VaR[t-1] + beta[3] * abs(ret[t-1])
+    }
+  } else if (model == "AS") {
+    for(t in 2:N) {
+      VaR[t] = beta[1] + beta[2] * VaR[t-1] + (beta[3] * (ret[t-1] > 0) + beta[4] * (ret[t-1] < 0)) * abs(ret[t-1])
+    }
+  } else if (model == "IG") {
+    for(t in 2:N) { 
+      VaR[t] = - sqrt(abs(beta[1] + beta[2] * VaR[t-1]^2 + beta[3] * ret[t-1]^2))
+    }
+  } else if (model == "IGm") {
+    for(t in 2:N) {
+      VaR[t] = beta[4] - sqrt(abs(beta[1] + beta[2] * (VaR[t-1] - beta[4])^2 + beta[3] * (ret[t-1] - beta[4])^2))
+    }
+  } else if (model == "GjG") {
+    for(t in 2:N) {
+      VaR[t] = -sqrt(abs(beta[1] + beta[2] * VaR[t-1]^2 + beta[3] * (ret[t-1]>0)^2 + beta[4] *(ret[t-1]<0)^2))
+    }
+  } else if (model == "GjGm") {
+    for(t in 2:N) {
+      VaR[t] = beta[5]-sqrt(abs(beta[1] + beta[2] * (VaR[t-1]-beta[5])^2 + beta[3] * ((ret[t-1]-beta[5])>0)^2 + beta[4] *((ret[t-1]-beta[5])<0)^2))
+    }
+  } else if (model == "SAVm") {
+    for(t in 2:N) {
+      VaR[t] = beta[4] * (1 - beta[2]) + beta[1] + beta[2] * VaR[t-1] + beta[3] * abs(ret[t-1] - beta[4])
+    }
+  } else if (model == "ASm") {
+    for(t in 2:N) {
+      VaR[t] = beta[5] * (1 - beta[2]) + beta[1] + beta[2] * VaR[t-1] + beta[3] * ((ret[t-1]-beta[5]) > 0) + beta[4] * ((ret[t-1]-beta[5]) < 0)
+    }
+  }
+  return(VaR)
+}
+#####################################################################
+####################################################################
+QL = function(beta, ret, tau, model, G){
+  
+  N = length(ret)
+  if(N < 300) stop("Length of ret should be at least 300")
+  if (!model %in% c("AD", "SAV", "AS", "IG", "IGm", "GjG", "GjGm", "SAVm", "ASm")) stop("Wrong MODEL selected")
+  
+  VaR = CAViaR.fun(beta, ret, tau, model, G)
+  
+  Hit = ifelse(ret <= VaR, 1, 0)
+  out = (tau - Hit) * (ret - VaR)
+  
+  return(mean(out))
+}
+#######################################################
+######################################################
+####################################################
+est.CAViaR = function(ret, tau, model, n.start, G) {
+  
+  if(missing(n.start)) n.start = 10
+  if(model == "AD") start_value = matrix(runif(n.start, -1, 1), n.start, 1)
+  if(model == "SAV") start_value = matrix(runif(n.start * 3, -1, 1), n.start, 3)
+  if(model == "AS") start_value = matrix(runif(n.start * 4, -1, 1), n.start, 4)
+  if(model == "IG") start_value = matrix(runif(n.start * 3, -1, 1), n.start, 3)
+  if(model == "IGm") start_value = matrix(runif(n.start * 4, -1, 1), n.start, 4)
+  if(model == "SAVm") start_value = matrix(runif(n.start * 4, -1, 1), n.start, 4)
+  if(model == "ASm") start_value = matrix(runif(n.start * 5, -1, 1), n.start, 5)
+  if(model == "GjG") start_value = matrix(runif(n.start * 4, -1, 1), n.start, 4)
+  if(model == "GjGm") start_value = matrix(runif(n.start * 5, -1, 1), n.start, 5)
+  QL_value = matrix(NA, n.start, 1)
+  pars_vec = matrix(NA, n.start, ncol(start_value))
+  
+  if(missing(G)) G = 10
+  if(model == "AD") {
+    method = "Brent"
+    low = -10
+    up = 10
+  } else {
+    method = "Nelder-Mead"
+    low = -Inf
+    up = Inf
+  }
+  
+  for(i in 1:n.start){
+    optim.out = optim(start_value[i,], fn = QL, ret = ret, tau = tau, model = model, G = G, 
+                      method = method, lower = low, upper = up, control = list(reltol = 1e-10, abstol = 1e-10, maxit = 1e4)) 
+    QL_value[i] = optim.out$value
+    pars_vec[i,] = optim.out$par
+  }
+  QL_max = QL_value[which.min(QL_value)]
+  pars_optim = pars_vec[which.min(QL_value),]
+  names(pars_optim) = paste("beta", 1:length(pars_optim), sep = "")
+  
+  fitted_VaR = CAViaR.fun(pars_optim, ret = ret, tau = tau, model = model, G = G)
+  Hit = ifelse(ret <= fitted_VaR, 1, 0)
+  
+  cat("Quantile level:", tau,
+      "\nCoefficients:", pars_optim,
+      "\nQL:", QL_max,
+      "\nProportion of VaR violations:", mean(Hit))
+  
+  out = list(tau = tau, pars = pars_optim, QL = QL_max, fitted = fitted_VaR, VaR_violations = Hit)
+  return(out)
+  }
+##############################################à
+
+######ok
+VaR_IGm_05 <- est.CAViaR(ret = Bitcoin, tau = 0.05, model = "IGm", n.start = 10)
+VaR_IGm05 <- CAViaR.fun(beta = VaR_IGm_05$pars, ret = Bitcoin.ret, tau = 0.05, model = "IGm", G=10)
+VaR_IGm_01 <- est.CAViaR(ret = Bitcoin, tau = 0.01, model = "IGm", n.start = 10)
+VaR_IGm01 <- CAViaR.fun(beta = VaR_IGm_01$pars, ret = Bitcoin.ret, tau = 0.01, model = "IGm", G=10)
+#
+VaR_ASm_05 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.05, model = "ASm", n.start = 10)
+VaR_ASm05 <- CAViaR.fun(beta = VaR_ASm_05$pars, ret = Bitcoin.ret, tau = 0.05, model = "ASm", G=10)
+VaR_ASm_01 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.01, model = "ASm", n.start = 10)
+VaR_ASm01 <- CAViaR.fun(beta = VaR_ASm_01$pars, ret = Bitcoin.ret, tau = 0.01, model = "ASm", G=10)
+######ok
+VaR_SAVm_05 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.05, model = "SAVm", n.start = 10)
+VaR_SAVm05 <- CAViaR.fun(beta = VaR_SAVm_05$pars, ret = Bitcoin.ret, tau = 0.05, model = "SAVm", G=10)
+VaR_SAVm_01 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.01, model = "SAVm", n.start = 10)
+VaR_SAVm01 <- CAViaR.fun(beta = VaR_SAVm_01$pars, ret = Bitcoin.ret, tau = 0.01, model = "SAVm", G=10)
+#
+VaR_GjG_05 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.05, model = "GjG", n.start = 10)
+VaR_GjG05 <- CAViaR.fun(beta = VaR_GjG_05$pars, ret = Bitcoin.ret, tau = 0.05, model = "SAVm", G=10)
+VaR_GjG_01 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.01, model = "GjG", n.start = 10)
+VaR_GjG01 <- CAViaR.fun(beta = VaR_GjG_01$pars, ret = Bitcoin.ret, tau = 0.01, model = "SAVm", G=10)
+#
+VaR_GjGm_05 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.05, model = "GjGm", n.start = 10)
+VaR_GjGm05 <- CAViaR.fun(beta = VaR_GjGm_05$pars, ret = Bitcoin.ret, tau = 0.05, model = "GjGm", G=10)
+VaR_GjGm_01 <- est.CAViaR(ret = Bitcoin.ret, tau = 0.01, model = "GjGm", n.start = 10)
+VaR_GjGm01 <- CAViaR.fun(beta = VaR_GjGm_01$pars, ret = Bitcoin.ret, tau = 0.01, model = "GjGm", G=10)
+
+
+
+alpha=0.05
+backtest05_AD <-BacktestVaR(Bitcoin.ret, VaR_Bitcoin_AD05, alpha, Lags = 4)
+backtest05_AS <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_AS05, alpha, Lags = 4)
+backtest05_SAV <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_SAV05, alpha, Lags = 4)
+backtest05_IG <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_IG05, alpha, Lags = 4)
+backtest05_Qreg <- BacktestVaR(Bitcoin.ret, VaR_Qreg05, alpha, Lags = 4)
+backtest05_Qregalt <- BacktestVaR(Bitcoin.ret, VaR_Qregalt05, alpha, Lags = 4)
+
+
+calcoli <-data.frame(VaR_SAVm05, VaR_SAVm01, VaR_IGm05, VaR_IGm01, VaR_GjGm05, VaR_GjGm01, VaR_ASm05, VaR_ASm01)
+write.xlsx(calcoli, "C:\\Users\\Daniel192\\Desktop\\TESINA SERIE STORICHE\\ELABORAZIONI\\Quarto-CAViars\\calcoli.xlsx")
+
+VaR_ASm05<-calcolo_VAR$VaR_Asm05
+VaR_ASm01<-calcolo_VAR$VaR_Asm01
+VaR_IGm05<-calcolo_VAR$VaR_IGm05
+VaR_IGm01<-calcolo_VAR$VaR_IGm01
+VaR_SAVm05<-calcolo_VAR$VaR_SAVm05
+VaR_SAVm01<-calcolo_VAR$VaR_SAVm01
+VaR_GjG05<-calcolo_VAR$VaR_Gjg05
+VaR_GjG01<-calcolo_VAR$VaR_Gjg01
+VaR_GjGm05<-calcolo_VAR$VaR_Gjgm05
+VaR_GjGm01<-calcolo_VAR$VaR_Gjgm01
+
+VaR_IGm05<-na.omit(VaR_IGm05)
+VaR_IGm01<-na.omit(VaR_IGm01)
+VaR_SAVm05<-na.omit(VaR_SAVm05)
+VaR_SAVm01<-na.omit(VaR_SAVm01)
+
+VaR_IGm05<-c(VaR_IGm05, mean(VaR_IGm05))
+VaR_IGm01<-c(VaR_IGm01, mean(VaR_IGm01))
+VaR_SAVm05<-c(VaR_SAVm05, mean(VaR_SAVm05))
+VaR_SAVm01<-c(VaR_SAVm01, mean(VaR_SAVm01))
+
+##############################################################à
+alpha=0.05
+backtest05_IGm <-BacktestVaR(Bitcoin.ret, VaR_IGm05, alpha, Lags = 4)
+backtest05_ASm <- BacktestVaR(Bitcoin.ret, VaR_ASm05, alpha, Lags = 4)
+backtest05_SAVm <- BacktestVaR(Bitcoin.ret, VaR_SAVm05, alpha, Lags = 4)
+backtest05_Gj <- BacktestVaR(Bitcoin.ret, VaR_GjG05, alpha, Lags = 4)
+backtest05_Gjm <- BacktestVaR(Bitcoin.ret, VaR_GjGm05, alpha, Lags = 4)
+
+IGm05<-c(mean(VaR_IGm05), backtest05_IGm$LRuc[2], backtest05_IGm$LRcc[2], backtest05_IGm$AE, backtest05_IGm$DQ[2], sd(VaR_IGm05))
+ASm05<-c(mean(VaR_ASm05), backtest05_ASm$LRuc[2], backtest05_ASm$LRcc[2], backtest05_ASm$AE, backtest05_ASm$DQ[2], sd(VaR_ASm05))
+SAVm05<-c(mean(VaR_SAVm05), backtest05_SAVm$LRuc[2], backtest05_SAVm$LRcc[2], backtest05_SAVm$AE, backtest05_SAVm$DQ[2], sd(VaR_SAVm05))
+GJ05<-c(mean(VaR_GjG05), backtest05_Gj$LRuc[2], backtest05_Gj$LRcc[2], backtest05_Gj$AE, backtest05_Gj$DQ[2], sd(VaR_GjG05))
+GJm05<-c(mean(VaR_GjGm05), backtest05_Gjm$LRuc[2], backtest05_Gjm$LRcc[2], backtest05_Gjm$AE, backtest05_Gjm$DQ[2], sd(VaR_GjGm05))
+
+#
+#
+alphaa=0.01
+backtest01_IGm <-BacktestVaR(Bitcoin.ret, VaR_IGm01, alphaa, Lags = 4)
+backtest01_ASm <- BacktestVaR(Bitcoin.ret, VaR_ASm01, alphaa, Lags = 4)
+backtest01_SAVm <- BacktestVaR(Bitcoin.ret, VaR_SAVm01, alphaa, Lags = 4)
+backtest01_Gj <- BacktestVaR(Bitcoin.ret, VaR_GjG01, alphaa, Lags = 4)
+backtest01_Gjm <- BacktestVaR(Bitcoin.ret, VaR_GjGm01, alphaa, Lags = 4)
+
+IGm01<-c(mean(VaR_IGm01), backtest01_IGm$LRuc[2], backtest01_IGm$LRcc[2], backtest01_IGm$AE, backtest01_IGm$DQ[2], sd(VaR_IGm01))
+ASm01<-c(mean(VaR_ASm01), backtest01_ASm$LRuc[2], backtest01_ASm$LRcc[2], backtest01_ASm$AE, backtest01_ASm$DQ[2], sd(VaR_ASm01))
+SAVm01<-c(mean(VaR_SAVm01), backtest01_SAVm$LRuc[2], backtest01_SAVm$LRcc[2], backtest01_SAVm$AE, backtest01_SAVm$DQ[2], sd(VaR_SAVm01))
+GJ01<-c(mean(VaR_GjG01), backtest01_Gj$LRuc[2], backtest01_Gj$LRcc[2], backtest01_Gj$AE, backtest01_Gj$DQ[2], sd(VaR_GjG01))
+GJm01<-c(mean(VaR_GjGm01), backtest01_Gjm$LRuc[2], backtest01_Gjm$LRcc[2], backtest01_Gjm$AE, backtest01_Gjm$DQ[2], sd(VaR_GjGm01))
+
+Tabella_Backtesting052<-data.frame(rbind(IGm=IGm05, ASm=ASm05, SAVm=SAVm05, GJ=GJ05, GJm=GJm05))
+colnames(Tabella_Backtesting052) <- c("Mean", "LRuc", "LRcc", "AE", "DQ", "SD")
+
+Tabella_Backtesting012<-data.frame(rbind(IGm=IGm01, ASm=ASm01, SAVm=SAVm01, GJ=GJ01, GJm=GJm01))
+colnames(Tabella_Backtesting012) <- c("Mean", "LRuc", "LRcc", "AE", "DQ", "SD")
+
+print(xtable(Tabella_Backtesting052, type = 'Latex'))
+print(xtable(Tabella_Backtesting012, type = 'Latex'))
+
+
+
---
+++ Test__.R
@@ -0,0 +1,166 @@
+setwd("C:\\Users\\Daniel192\\Desktop\\TESINA SERIE STORICHE\\ELABORAZIONI\\Terzo-2")
+library(xts)
+library(quantreg)
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+require(quantmod)
+library(GAS)
+library(xtable)
+library(PerformanceAnalytics)
+library(quantmod)
+source("CAViaR.R")
+ret_crypto_index_comm <- read_excel("ret_crypto_index_comm.xlsx")
+ret_crypto_index_comm$Date <- as.Date(ret_crypto_index_comm$Date)
+calcolo_VAR_definitivo <- read_excel("calcoloVAR_definitivo.xlsx")
+calcolo_VAR_definitivo$Date <- as.Date(calcolo_VAR$Date)
+################################
+########### CON I NOSTRI DATI
+#################################
+##############################
+##noi abbiamo già dei rendimenti, partiamo da lì
+Bitcoin.ret <- xts(ret_crypto_index_comm[, 2], order.by = ret_crypto_index_comm$Date)
+Time = index(Bitcoin.ret)
+range(Time)
+
+
+tau = 0.05 # quantile level
+ret = coredata(Bitcoin.ret)
+n.start = 10 # number of restarts
+fit_AD_10 = est.CAViaR(ret = Bitcoin.ret, tau = tau, model = "AD", n.start = n.start, G = 10) 
+fit_AD_1e10 = est.CAViaR(ret = Bitcoin.ret, tau = tau, model = "AD", n.start = n.start, G = 1e10) 
+fit_SAV = est.CAViaR(ret = Bitcoin.ret, tau = tau, model = "SAV", n.start = n.start) 
+fit_AS = est.CAViaR(ret = Bitcoin.ret, tau = tau, model = "AS", n.start = n.start) 
+fit_IG = est.CAViaR(ret = Bitcoin.ret, tau = tau, model = "IG", n.start = n.start) 
+###valori che già posseggo nell'excel perciò posso
+Qreg <- (data=calcolo_VAR$VaR_QuantileReg)
+
+VaR_Bitcoin_SAV05<- (data=calcolo_VAR_definitivo$VaR_BItcoin_SAV05)
+
+
+VaR_Bitcoin_IG05<- (data=calcolo_VAR_definitivo$VaR_BItcoin_IG05)
+
+VaR_Bitcoin_AD05<- (data=calcolo_VAR_definitivo$VaR_BItcoin_AD05)
+
+VaR_Bitcoin_AS05<- (data=calcolo_VAR_definitivo$VaR_BItcoin_AS05)
+
+VaR_Qreg05<- (data=calcolo_VAR_definitivo$VaR_Qreg_05)
+
+VaR_Qregalt05<- (data=calcolo_VAR_definitivo$VaR_Qregalt_05)
+
+
+##############################################################
+VaR_Bitcoin_SAV01<- (data=calcolo_VAR_definitivo$VaR_BItcoin_SAV01)#
+
+VaR_Bitcoin_IG01<- (data=calcolo_VAR_definitivo$VaR_BItcoin_IG01)#
+
+VaR_Bitcoin_AD01<- (data=calcolo_VAR_definitivo$VaR_BItcoin_AD01)#
+
+VaR_Bitcoin_AS01<- (data=calcolo_VAR_definitivo$VaR_BItcoin_AS01)#
+
+VaR_Qreg01<- (data=calcolo_VAR_definitivo$VaR_Qreg_01)#
+
+VaR_Qregalt01<- (data=calcolo_VAR_definitivo$VaR_Qregalt_01)#
+
+VaR_Qreg01<-na.omit(VaR_Qreg01)
+VaR_Qreg01<-c(VaR_Qreg01, mean(VaR_Qreg01))
+
+VaR_Qregalt01<-na.omit(VaR_Qregalt01)
+VaR_Qregalt01<-c(VaR_Qregalt01, mean(VaR_Qregalt01))
+
+
+
+
+plot.ts(Bitcoin.ret)
+lines(VaR_Bitcoin_SAV05, type = "l", col = "blue")
+lines(VaR_Bitcoin_AD05, type = "l", col = "orange")
+lines(VaR_Bitcoin_AS05, type = "l", col = "green")
+lines(VaR_Bitcoin_IG05, type = "l", col = "violet")
+
+legend("bottomleft", legend = c("VaR_SAV", "VaR_AD", "VaR_AS", "VaR_IG"),
+       col = c("blue","orange", "green", "yellow","violet"), 
+       lty = 1, 
+       cex = 0.8)
+
+
+plot.ts(Bitcoin.ret)
+lines(VaR_Qreg05, type = "l", col = "red")
+
+legend("topright", 
+       legend = c("VaR_AD_plot", "VaR_AS_plot", "VaR_IG_plot", "Qreg"),
+       col = c("orange", "green", "yellow","violet"), 
+       lty = 1, 
+       cex = 0.8)
+
+
+
+VaR_Qreg05<-na.omit(VaR_Qreg05)
+VaR_Qreg05<- c(VaR_Qreg05, mean(VaR_Qreg05))
+
+VaR_Qregalt05<-na.omit(VaR_Qregalt05)
+VaR_Qregalt05<- c(VaR_Qregalt05, mean(VaR_Qregalt05))
+VaR_Qregalt01<-na.omit(VaR_Qregalt01)
+VaR_Qregalt01<- c(VaR_Qregalt01, mean(VaR_Qregalt01))
+
+mean(VaR_Bitcoin_IG05)
+mean(VaR_Bitcoin_AD05)
+mean(VaR_Bitcoin_AS05)
+mean(VaR_Bitcoin_SAV05)
+mean(VaR_Bitcoin_Qreg05)
+mean(VaR_Bitcoin_Qregalt05)
+
+
+mean(VaR_Bitcoin_AD01)
+mean(VaR_Bitcoin_AS01)
+mean(VaR_Bitcoin_SAV01)
+mean(VaR_Bitcoin_IG01)
+mean(VaR_Qreg01)
+mean(VaR_Qregalt01)
+
+
+alpha=0.05
+backtest05_AD <-BacktestVaR(Bitcoin.ret, VaR_Bitcoin_AD05, alpha, Lags = 4)
+backtest05_AS <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_AS05, alpha, Lags = 4)
+backtest05_SAV <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_SAV05, alpha, Lags = 4)
+backtest05_IG <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_IG05, alpha, Lags = 4)
+backtest05_Qreg <- BacktestVaR(Bitcoin.ret, VaR_Qreg05, alpha, Lags = 4)
+backtest05_Qregalt <- BacktestVaR(Bitcoin.ret, VaR_Qregalt05, alpha, Lags = 4)
+
+names(backtest05_AD)
+
+backtest05_AD$LRuc[2]
+##chiedo solo i pvalue delle statistiche
+##TAU=0.05
+ad05<-c(mean(VaR_Bitcoin_AD05), backtest05_AD$LRuc[2], backtest05_AD$LRcc[2], backtest05_AD$AE, backtest05_AD$DQ[2], sd(VaR_Bitcoin_AD05))
+as05<-c(mean(VaR_Bitcoin_AS05), backtest05_AS$LRuc[2], backtest05_AS$LRcc[2], backtest05_AS$AE, backtest05_AS$DQ[2], sd(VaR_Bitcoin_AS05))
+sav05<-c(mean(VaR_Bitcoin_SAV05), backtest05_SAV$LRuc[2], backtest05_SAV$LRcc[2], backtest05_SAV$AE, backtest05_SAV$DQ[2], sd(VaR_Bitcoin_SAV05))
+ig05<-c(mean(VaR_Bitcoin_IG05), backtest05_IG$LRuc[2], backtest05_IG$LRcc[2], backtest05_IG$AE, backtest05_IG$DQ[2], sd(VaR_Bitcoin_IG05))
+qr05<-c(mean(VaR_Qreg05), backtest05_Qreg$LRuc[2], backtest05_Qreg$LRcc[2], backtest05_Qreg$AE, backtest05_Qreg$DQ[2], sd(VaR_Qreg05))
+qra05<-c(mean(VaR_Qregalt05), backtest05_Qregalt$LRuc[2], backtest05_Qregalt$LRcc[2], backtest05_Qregalt$AE, backtest05_Qregalt$DQ[2], sd(VaR_Qregalt05))
+
+
+alpha1=0.01
+backtest01_AD <-BacktestVaR(Bitcoin.ret, VaR_Bitcoin_AD01, alpha1, Lags = 4)
+backtest01_AS <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_AS01, alpha1, Lags = 4)
+backtest01_SAV <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_SAV01, alpha1, Lags = 4)
+backtest01_IG <- BacktestVaR(Bitcoin.ret, VaR_Bitcoin_IG01, alpha1, Lags = 4)
+backtest01_Qreg <- BacktestVaR(Bitcoin.ret, VaR_Qreg01, alpha1, Lags = 4)
+backtest01_Qregalt <- BacktestVaR(Bitcoin.ret, VaR_Qregalt01, alpha1, Lags = 4)
+
+#TAU=0.01
+ad01<-c(mean(VaR_Bitcoin_AD01), backtest01_AD$LRuc[2], backtest01_AD$LRcc[2], backtest01_AD$AE, backtest01_AD$DQ[2], sd(VaR_Bitcoin_AD01))
+as01<-c(mean(VaR_Bitcoin_AS01), backtest01_AS$LRuc[2], backtest01_AS$LRcc[2], backtest01_AS$AE, backtest01_AS$DQ[2], sd(VaR_Bitcoin_AS01))
+sav01<-c(mean(VaR_Bitcoin_SAV01), backtest01_SAV$LRuc[2], backtest01_SAV$LRcc[2], backtest01_SAV$AE, backtest01_SAV$DQ[2], sd(VaR_Bitcoin_SAV01))
+ig01<-c(mean(VaR_Bitcoin_IG01), backtest01_IG$LRuc[2], backtest01_IG$LRcc[2], backtest01_IG$AE, backtest01_IG$DQ[2], sd(VaR_Bitcoin_IG01))
+qr01<-c(mean(VaR_Qreg01), backtest01_Qreg$LRuc[2], backtest01_Qreg$LRcc[2], backtest01_Qreg$AE, backtest01_Qreg$DQ[2], sd(VaR_Qreg01))
+qra01<-c(mean(VaR_Qregalt01), backtest01_Qregalt$LRuc[2], backtest01_Qregalt$LRcc[2], backtest01_Qregalt$AE, backtest01_Qregalt$DQ[2], sd(VaR_Qregalt01))
+
+
+Tabella_Backtesting05<-data.frame(rbind(AD=ad05, AS=as05, SAV=sav05, IG=ig05, QR=qr05, QR2=qra05 ))
+colnames(Tabella_Backtesting05) <- c("Mean", "LRuc", "LRcc", "AE", "DQ", "SD")
+
+Tabella_Backtesting01<-data.frame(rbind(AD=ad01, AS=as01, SAV=sav01, IG=ig01, QR=qr01, QR2=qra01 ))
+colnames(Tabella_Backtesting01) <- c("Mean", "LRuc", "LRcc", "AE", "DQ", "SD")
+
+print(xtable(Tabella_Backtesting05, type = 'Latex'))
+print(xtable(Tabella_Backtesting01, type = 'Latex'))
